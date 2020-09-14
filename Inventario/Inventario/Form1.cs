ca549408e38541e9a156651de251f55cc2990c36
jeffr <jeffr@JEFFGR>
2020-09-14 11:37:14 -0600

    Seguimiento

--- Inventario/Inventario/Form1.cs
+++ Inventario/Inventario/Form1.cs
@@ -19,7 +19,13 @@ namespace Inventario
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            var producto1 = new Producto();
+            producto1.Id = 001;
+            producto1.Descripcion = "sepa dios";
+            producto1.Precio = 1500.00;
+            producto1.FechaC = DateTime.Now;
+
+            MessageBox.Show(producto1.Id+ "" + "" + producto1.Descripcion + "" + producto1.Precio + "" + producto1.FechaC);
         }
     }
 }
